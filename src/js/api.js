69f089a372455d3d4b05cc4d72c57aa5d2b123a0
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-22 21:36:20 +0300

    api

--- src/js/api.js
+++ src/js/api.js
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-
-const refs = {
-  buttonSearch: document.querySelector('.search'),
-  searchIcon: document.querySelector('.icon-search'),
-  spinnerIcon: document.querySelector('.icon-spinner'),
-  searchForm: document.querySelector('.search-form'),
-  gallery: document.querySelector('.gallery'),
-};
-
 const API_KEY = '35413262-7ae9db1d2d6405a91836db282';
 const URL = 'https://pixabay.com/api';
 const searchQuery ="cat";
@@ -20,22 +11,20 @@ const options = {
 };
 
 
-async function getImages(searchQuery,page) {
+async getImages(searchQuery,page) {
   try{
-     const {data}  = await axios.get(
+     const { data } = await axios.get(
     `${URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
 
   // this.incrementPage();
-  console.log (data);
   return data;
-
   }catch (error) {
     console.error(error);
   }
  
 }
-getImages(searchQuery,page);
+
 // incrementPage() {
 //   this.page += 1;
 // }

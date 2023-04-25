df457133b406eaaaaf542ee5fa5a1c57386b91a5
smiadv2022 <112901361+smiadv2022@users.noreply.github.com>
2023-04-25 12:13:23 +0000

    Deploying to gh-pages from @ smiadv2022/goit-js-hw-11v1@b8faaa3c30479afff20a3e117154eded324f0d35 🚀

---
+++ index.6694527f.js
@@ -0,0 +1,2 @@
+[document.querySelector(".search"),document.querySelector(".icon-search"),document.querySelector(".icon-spinner"),document.querySelector(".search-form"),document.querySelector(".gallery")][0].addEventListener("submit",(function(e){e.preventDefault();var r=e.currentTarget.elements.searchQuery.value.trim();console.log("txt",r)}));
+//# sourceMappingURL=index.6694527f.js.map

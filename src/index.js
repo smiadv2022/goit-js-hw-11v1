116782f450c97fd7c2d08266ac573699e5e5c71a
smiadv2022 <msorokina@spacemedia.com.ua>
2023-04-26 15:08:47 +0300

    reduce

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ const refs = {
   gallery: document.querySelector('.gallery'),
 };
 
-refs.buttonSearch.addEventListener('submit', onSearch);
+refs.searchForm.addEventListener('submit', onSearch);
 
-const API_KEY = '35628510-01ea92234f245f2047fa1b595З';
-// const API_KEY = '35413262-7ae9db1d2d6405a91836db282';
-// 35628510-01ea92234f245f2047fa1b595З
+const API_KEY = '35628510-01ea92234f245f2047fa1b595';
+                 
+
 const URL = 'https://pixabay.com/api';
-// const searchQuery ="cat";
+
 const page =1;
 const options = {
   headers: {
@@ -27,28 +27,46 @@ const options = {
 };
 
 
-// async function getImages(search,page) {
-//   try{
-//      const {data}  = await axios.get(
-//     `${URL}/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
-//   );
+async function getImages(search,page) {
+  try{
+     const {data}  = await axios.get(
+    `${URL}/?key=${API_KEY}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  );
 
-//   // this.incrementPage();
-//   console.log ("data",data);
-//   return data;
+  // this.incrementPage();
+  console.log ("data",data);
+  return data;
 
-//   }catch (error) {
-//     console.error(error);
-//   }
+  }catch (error) {
+    console.error(error);
+  }
  
-// }
+}
 
 function onSearch(event) {
   event.preventDefault();
-
+  const form = event.currentTarget;
   const newSearchQuery = event.currentTarget.elements.searchQuery.value.trim();
 
-  console.log ("txt",  newSearchQuery );
+  console.log('txt', page, newSearchQuery);
+  const dataSearch = getImages(newSearchQuery, page)
+    .then(({ hits }) => {
+      console.log(hits);
+      console.log(hits.length);
+      if (hits.length === 0) {
+        throw new Error('no data');
+      }
 
+      hits.reduce((markup, hit)=> createMarkup(hit),"")
+    })
+    .catch(onError)
+    .finally(() => form.reset());
+  console.log('search', dataSearch);
 }
 
+function onError(err) {
+  console.log(err);
+}
+function createMarkup(hit){
+  console.log(hit);
+}
